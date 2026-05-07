8eee39c55ba857e8a2b8be9156617ec622f0566a
nothingwindsky <56858014+nothingwindsky@users.noreply.github.com>
2026-05-07 07:42:28 -0700

    Create bio.js

---
+++ bio.js
@@ -0,0 +1,28 @@
+const bios = {
+  hkoubliette: [
+  ],
+
+  tomacgau: [
+    "has been diagnosed with autism by three different doctors. He still doesn't buy it.",
+    "is cultivating disinterest.",
+    "ghostwrote Hillbilly Elegy.",
+    "is a casualty of the food allergies to gnosticism pipeline",
+    "has no mouth but he just can't stop smiling!",
+    "is slitting on your grave to lube it up.",
+    "is not living in the moment.",
+    "operates a rapidly expanding kudzu farm."
+  ]
+};
+
+function getRandomBio(author) {
+  const options = bios[author];
+
+  if (!options) return "";
+
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+document.querySelectorAll(".bio").forEach(element => {
+  const author = element.dataset.author;
+  element.textContent = getRandomBio(author);
+});
